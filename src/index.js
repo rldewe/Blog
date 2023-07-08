e0feb3375f8659895d9cc935678ea473b81c2d67
rldewe <04srishtiy@gmail.com>
2023-07-08 15:06:03 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {Navbar,Home,Footer} from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ''}>
       <App />
 
     </BrowserRouter>
